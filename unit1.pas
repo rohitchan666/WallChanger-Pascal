dbf6f0865fe9eb261f50c60502fe601b60c97b13
rohitchan666 <120973690+rohitchan666@users.noreply.github.com>
2022-12-19 00:33:44 +0530

    Add files via upload

---
+++ unit1.pas
@@ -0,0 +1,46 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, EditBtn,
+  StdCtrls, Spin, ComCtrls, windows;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+
+begin
+
+     SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, pchar(Edit1.Text), SPIF_SENDWININICHANGE);
+
+end;
+
+
+
+end.
+
